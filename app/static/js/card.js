03560507f20a2340ddd591fcb7700c220bcc5553
Dong Zhou <zhou.dong@gmail.com>
2017-07-24 01:20:01 -0400

    use vue component for each job

--- app/static/js/card.js
+++ app/static/js/card.js
@@ -1,4 +1,21 @@
 
-Vue.component('card', {
-      template: "<p>This is the application's navbar</p>"
+Vue.component('job-card', {
+  props: ['job'],
+  template: ` <div class="col-sm-3">
+                <div class="panel panel-default">
+                <div class="panel-heading">
+                  <h3 class="panel-title">
+                    {{job.id}} {{ job.status }} 
+                  </h3>
+                </div>
+                <div class="panel-body">
+                  <p><span class="badge alert-info"> priority </span>
+                    {{ job.priority }}
+                  </p>
+                  <p><span class="badge alert-danger"> user id: </span>
+                     <strong> {{job.user_id}} </strong>
+                  </p>
+                </div>
+              </div>
+              </div> `
 })
