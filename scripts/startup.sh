2cb05d8c82ba3a86e25c393c0774f4abe9da0fad
nos <nosarthur@users.noreply.github.com>
2017-07-30 16:58:10 -0400

    Update startup.sh

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -1,3 +1,11 @@
+#!/bin/bash
+
+echo "prerequisite"
 pip install -r requirements.txt
 
+echo "start the worker monitoring"
+
+
+# start the http server
 gunicorn cloud-harness:app -p cloud-harness.pid -D
+
